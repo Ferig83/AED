61fc2f687166fd45a5da04072b5b93d2d2278048
Fernando Gustavo Riganti <57960943+Ferig83@users.noreply.github.com>
2020-04-20 11:41:52 -0300

    Create hello.cpp

---
+++ TP0/hello.cpp
@@ -0,0 +1,14 @@
+/* ---------------------------------------------\
+						
+  Trabajo Práctico 0 - "Hola, Mundo!"  	
+						
+	Autor: Fernando Riganti			
+	Legajo: 156.149-0			
+						
+\--------------------------------------------- */
+
+#include <iostream>   
+
+int main() {
+		std::cout << "Hola!\neste es un programa en C++\nChau!\n";
+	}
