bb278fe2fa2cfc4234a5710959a26823e6a59241
Fernando Gustavo Riganti <57960943+Ferig83@users.noreply.github.com>
2020-04-25 22:15:38 +0000

    Create main.cpp

---
+++ DD-Adición/main.cpp
@@ -0,0 +1,23 @@
+/*** Adición ************** 
+ *
+ *  por Fernando Riganti
+ *  25/04/2020
+ *
+ *  
+ **************************/
+
+
+#include <iostream>
+
+int main ()
+{
+	int numero1, numero2, resultado;
+
+	std::cout << "Ingrese el primer número: ";
+	std::cin >> numero1;
+	std::cout << "Ingrese el segundo número: ";
+	std::cin >> numero2;
+	resultado = numero1 + numero2;
+	std::cout << "El resultado es: " << resultado << std::endl;
+	return 0;
+}
