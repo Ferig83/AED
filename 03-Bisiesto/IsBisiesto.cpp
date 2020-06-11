50b3efe1a5031cc315e362b8828297581e6536e5
Fernando Gustavo Riganti <57960943+Ferig83@users.noreply.github.com>
2020-06-11 00:00:05 -0300

    Create IsBisiesto.cpp

---
+++ 03-Bisiesto/IsBisiesto.cpp
@@ -0,0 +1,34 @@
+/******************************************
+*
+*  Trabajo Práctico N° 3 - Años Bisiestos
+*
+*  por Fernando Gustavo Riganti (159.146-0)
+*
+*
+*******************************************/
+
+
+// Atención! Asumimos que se ingresan años superiores a 1852, no negativos. Años anteriores a
+// 1852 no son ni bisiestos ni no bisiestos.
+
+#include <cassert>
+
+
+bool IsBisiesto(int year);
+
+
+int main ()
+{	   	
+	assert(IsBisiesto(2000) == true);
+	assert(IsBisiesto(2004) == true);
+	assert(IsBisiesto(1990) == false);
+	assert(IsBisiesto(1992) == true);
+	assert(IsBisiesto(2020) == true);
+	assert(IsBisiesto(1900) == false);
+}
+
+bool IsBisiesto(int year)
+{
+	return year % 4 == 0 and ((year % 100 == 0 and year % 400 == 0) or !(year % 100 == 0));   
+}
+
